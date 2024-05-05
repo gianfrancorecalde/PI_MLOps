53a05968ad74935ce7ce000dc757d2a981ccc398
Gian Franco <francrecalde19998@gmail.com>
2024-05-05 16:47:00 -0300

    Logre desarollar el DF

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import numpy as np
-import FastAPI
-import BaseModel
 
-app = FastAPI()
+from fastapi import FastAPI
 
+app = FastAPI() #
 
-@app.get("/")
-async def root():
+games = pd.read_parquet('steam_Games')
+items = pd.read_parquet('user_items')
+reviews = pd.read_parquet('user_Reviews')
+
+@app.get("/PlayTimeGenre/{genero}")
+async def PlayTimeGenre(genero :str):
+    
+    games
     return {"message": "Hello World"}
-
-@app.post("/")
-def PlayTimeGenre(genero: str):
-    return 
